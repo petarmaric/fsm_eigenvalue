263b00f7dcf41b51eee5c0ce8ca1804985b7e7ca
Petar Marić <petar.maric@gmail.com>
2017-09-29 11:32:51 +0200

    Initial shot at creating a distutils/pip compatible setup.py

---
+++ setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='fsm_eigenvalue',
+    version='0.1.0.dev',
+    url='https://bitbucket.org/petar/fsm_eigenvalue',
+    license='BSD',
+    author='Petar Maric',
+    author_email='petarmaric@uns.ac.rs',
+    description='Console app and Python API implementing a generalization of '\
+                'eigenvalue problem within the harmonic coupled finite strip '\
+                'method, used for parametric modeling of static and dynamic '\
+                'inelastic buckling, free vibration, damage and failure in '\
+                'prismatic shell structures.',
+    long_description=open('README').read(),
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Physics',
+    ],
+    platforms='any',
+    packages=find_packages(),
+    install_requires=open('requirements.txt').read().splitlines(),
+)
